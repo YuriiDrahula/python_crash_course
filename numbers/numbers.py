2474f2ca622dc589f5ae6a66ec4cdf0c01ec2f44
Yura Drahula <ydrahula@gmail.com>
2022-07-01 15:00:54 +0300

    learning numbers

---
+++ numbers/numbers.py
@@ -0,0 +1,8 @@
+print(4 + 4)
+print(21 - 13)
+print(4 * 2)
+print(24 / 3)
+
+favorite_number = 97
+favorite_number_message = f"My favorite number is {favorite_number}"
+print(favorite_number_message)
