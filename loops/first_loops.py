a4200b4a3762638ef52da1b1b663af18e44355a4
Yura Drahula <ydrahula@gmail.com>
2022-07-02 12:09:54 +0300

    learning loops

---
+++ loops/first_loops.py
@@ -0,0 +1,9 @@
+favorite_pizza = ['4 cheeses', 'meat pizza', 'margarita', 'pepperoni']
+for pizza in favorite_pizza:
+    print(f'My favorite pizza is {pizza}.')
+print('I really like pizza!')
+
+pets = ['cat', 'dog', 'hamster', 'hedgehog']
+for pet in pets:
+    print(f'A {pet} would make a great pet.')
+print('Any of these animals would make a great pet.')
